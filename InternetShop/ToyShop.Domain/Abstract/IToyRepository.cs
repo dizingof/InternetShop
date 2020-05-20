27018c940b71f55b654b4963b8a66f4a946594e2
Roman.Nepomniashchyi <Roman.Nepomniashchyi@infopulse.com>
2020-05-20 21:30:49 +0300

    add interfaces

---
+++ InternetShop/ToyShop.Domain/Abstract/IToyRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToyShop.Domain.Entities;
+
+namespace ToyShop.Domain.Abstract
+{
+    public interface IToyRepository
+    {
+        IEnumerable<Toy> Toys { get; }
+    }
+}
