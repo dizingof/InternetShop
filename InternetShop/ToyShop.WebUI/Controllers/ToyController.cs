69c66928b06161328083e14ecfbbdef7a8625e4e
Roman.Nepomniashchyi <Roman.Nepomniashchyi@infopulse.com>
2020-05-22 18:13:21 +0300

    change routeconfig

---
+++ InternetShop/ToyShop.WebUI/Controllers/ToyController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ToyShop.Domain.Abstract;
+using ToyShop.Domain.Entities;
+
+namespace ToyShop.WebUI.Controllers
+{
+    public class ToyController : Controller
+    {
+        private IToyRepository toyRepository;
+        // GET: Toy
+        public ToyController(IToyRepository Repository)
+        {
+            toyRepository = Repository;
+        }
+        public ViewResult List()
+        {
+            return View(toyRepository.Toys);
+        }
+    }
+}
