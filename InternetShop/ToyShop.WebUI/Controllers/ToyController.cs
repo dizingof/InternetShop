f50dff8b088cc0dd0ef12c6b608f346bc31867b4
Roman.Nepomniashchyi <Roman.Nepomniashchyi@infopulse.com>
2020-05-25 21:23:22 +0300

    add pages to controller

--- InternetShop/ToyShop.WebUI/Controllers/ToyController.cs
+++ InternetShop/ToyShop.WebUI/Controllers/ToyController.cs
@@ -11,14 +11,17 @@ namespace ToyShop.WebUI.Controllers
     public class ToyController : Controller
     {
         private IToyRepository toyRepository;
-        // GET: Toy
+        public int pageSize = 3;
         public ToyController(IToyRepository Repository)
         {
             toyRepository = Repository;
         }
-        public ViewResult List()
+        public ViewResult List(int page = 1)
         {
-            return View(toyRepository.Toys);
+            return View(toyRepository.Toys
+                        .OrderBy(toy => toy.ToyId)
+                        .Skip((page - 1) * pageSize)
+                        .Take(pageSize));
         }
     }
 }
